96716d376106e05d1b6d4121201c4315092cb862
Howard (Aech Linden) Stearns <howard.stearns@gmail.com>
2024-05-04 14:36:30 -0700

    cleanup

--- index.mjs
+++ index.mjs
@@ -4,12 +4,16 @@ function transferrableError(error) { // An error object that we receive on our s
   return {name, message};
 }
 
-function dispatch({target,
-		   receiver = target,
-		   namespace = receiver,
+// Set up bidirectional communcations with target, returning a function (methodName, ...params) that will send to target.
+function dispatch({target = self,        // The window, worker, or other object to which we will postMessage.
+		   receiver = target,    // The window, worker, or other object of which WE will handle 'message' events from target.
+		   namespace = receiver, // An object that defines any methods that may be requested by target.
+
 		   origin = ((target !== receiver) && target.location.origin),
-		   targetLabel = origin || target.location?.href || target,
-		   dispatcherLabel = receiver.location?.href || receiver,
+
+		   dispatcherLabel = namespace.name || receiver.name || receiver.location?.href || receiver,
+		   targetLabel = target.name || origin || target.location?.href || target,
+
 		   log = () => null,
 		   warn:logwarn = console.warn.bind(console),
 		   error:logerror = console.error.bind(console)
@@ -18,28 +22,45 @@ function dispatch({target,
       messageId = 0,
       jsonrpc = '2.0',
       capturedPost = target.postMessage.bind(target), // In case (malicious) code later changes it.
-      // window.postMessage and friends takes a targetOrigin that we should supply.
-      // But other forms give error rather than ignoring the extra arg. So set the right form at initialization.
+      // window.postMessage and friends takes a targetOrigin that we supply.
+      // But worker.postMessage gives error rather than ignoring the extra arg. So set the right form at initialization.
       post = origin ? message => capturedPost(message, origin) : capturedPost;
-  log(dispatcherLabel, 'dispatch to', targetLabel);
 
-  receiver.addEventListener('message', async event => {
+  function request(method, ...params) { // Promise the result of method(...params) in target.
+    // We do a target.postMessage of a jsonrpc request, and resolve the promise with the response, matched by id.
+    // If the target happens to be set up by a dispatch like this one, it will respond with whatever it's
+    // namespace[method](...params) resolves to. We only send jsonrpc requests (with an id), not notifications,
+    // because there is no way to get errors back from a jsonrpc notification.
+    let id = ++messageId,
+	request = requests[id] = {};
+    // It would be nice to not leak request objects if they aren't answered.
+    return new Promise((resolve, reject) => {
+      log(dispatcherLabel, 'requesting', id, method, params, 'to', targetLabel);
+      Object.assign(request, {resolve, reject});
+      post({id, method, params, jsonrpc});
+    });
+  }
+
+  async function respond(event) { // Handle 'message' events that we receive from target.
     log(dispatcherLabel, 'got message', event.data, 'from', targetLabel, event.origin);
     let {id, method, params = [], result, error, jsonrpc:version} = event.data || {};
-    if (event.source && (event.source !== target)) return logerror(dispatcherLabel, 'got mismatched event from', targetLabel, event.origin);
+
+    // Noisily ignore messages that are not from the expect target or origin, or which are not jsonrpc.
+    if (event.source && (event.source !== target)) return logerror(dispatcherLabel, 'to', targetLabel,  'got message from', event.source);
     if (origin && (origin !== event.origin)) return logerror(dispatcherLabel, origin, 'mismatched origin', targetLabel, event.origin);
     if (version !== jsonrpc) return logwarn(`${dispatcherLabel} ignoring non-jsonrpc message ${JSON.stringify(event.data)}.`);
 
     if (method) { // Incoming request or notification from target.
       let error = null, result,
-	  args = Array.isArray(params) ? params : [params]; // Accept either form of params.
+          // jsonrpc request/notification can have positional args (array) or named args (a POJO).
+	  args = Array.isArray(params) ? params : [params]; // Accept either.
       try { // method result might not be a promise, so we can't rely on .catch().
-	result = await namespace[method](...args);
-      } catch (e) {
-	error = transferrableError(e);
-	if (!namespace[method] && !error.message.includes(method))
+        result = await namespace[method](...args); // Call the method.
+      } catch (e) { // Send back a clean {name, message} object.
+        error = transferrableError(e);
+        if (!namespace[method] && !error.message.includes(method))
 	  error.message = `${method} is not defined.`; // Be more helpful than some browsers.
-	else if (!error.message) // It happens. E.g., operational errors from crypto.
+        else if (!error.message) // It happens. E.g., operational errors from crypto.
 	  error.message = `${error.name || error.toString()} in ${method}.`;
       }
       let response = error ? {id, error, jsonrpc} : {id, result, jsonrpc};
@@ -47,23 +68,18 @@ function dispatch({target,
       return post(response);
     }
 
-    let request = requests[id]; // A response from target to our earlier outgoing request.
+    // Otherwise, it is a response from target to our earlier outgoing request.
+    let request = requests[id];  // Resolve or reject the promise that an an earlier request created.
     delete requests[id];
     if (!request) return logwarn(`${dispatcherLabel} ignoring response ${event.data}.`);
     if (error) request.reject(error);
     else request.resolve(result);
-  });
+  }
 
-  // FIXME: Don't leak promises when there is no response. Timeout? Return both request and notify? special arg? Some combination?
-  return function request(method, ...params) {
-    let id = ++messageId,
-	request = requests[id] = {};
-    return new Promise((resolve, reject) => {
-      Object.assign(request, {resolve, reject});
-      log(dispatcherLabel, 'requesting', id, method, params, 'to', targetLabel);
-      post({id, method, params, jsonrpc});
-    });
-  };
+  // Now set up the handler and return the function for the caller to use to make requests.
+  log(`${dispatcherLabel} will dispatch to ${targetLabel}`);
+  receiver.addEventListener('message', respond);
+  return request;
 }
 
 export default dispatch;
